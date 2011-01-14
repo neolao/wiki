84c16ffe1208360fd30ef2ac2626bc4a83d3a9dd
neolao <contact@neolao.com>
2011-01-14 18:38:05 +0100

    Add browse feature

--- www/.wiki/file.php
+++ www/.wiki/file.php
@@ -16,6 +16,7 @@ if (!is_file($filePath)) {
 }
 
 $filePath = realpath($filePath);
+$directoryPath = dirname($filePath);
 $content = file_get_contents($filePath);
 
 // Convert the raw content to wiki content
@@ -43,6 +44,35 @@ foreach ($availableLanguages as $language) {
     }
 }
 $hasCode = (count($languages) > 0);
+
+
+// Browse feature
+$folders = array();
+$files = array();
+if ($handle = opendir($directoryPath)) {
+    while (false !== ($name = readdir($handle))) {
+        if (!in_array($name, array('.', '..', '.htaccess', '.wiki'))) {
+            if (is_dir($directoryPath.'/'.$name)) {
+                $folders[$name] = '<li><a class="folder" href="'.$name.'">'.$name.'</a></li>';
+            } else {
+                $files[$name] = '<li><a class="file" href="'.$name.'">'.$name.'</a></li>';
+            }
+        }
+    }
+    closedir($handle);
+}
+ksort($folders);
+ksort($files);
+$browse = '<ul class="browse" style="-moz-column-count:$1;-webkit-column-count:$1;column-count:$1;">';
+foreach ($folders as $folder) {
+    $browse .= $folder;
+}
+foreach ($files as $file) {
+    $browse .= $file;
+}
+$browse .= '</ul>';
+$html = preg_replace('|<p>\[browse ?(\d*)?\]</p>|', $browse, $html);
+
 
 ?><!DOCTYPE html>
 <html>
